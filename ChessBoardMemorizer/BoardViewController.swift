49d422a0b8f31311ac9c08421bef4df299d50bca
Artur Honzawa <arturh@gmail.com>
2018-02-17 12:02:10 +0100

    Show bw checkerboard

--- ChessBoardMemorizer/BoardViewController.swift
+++ ChessBoardMemorizer/BoardViewController.swift
@@ -23,25 +23,53 @@ extension BoardViewController: UICollectionViewDataSource {
         return 1
     }
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int)
+        -> Int
+    {
         return 64
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath)
+        -> UICollectionViewCell
+    {
         let cell = collectionView.dequeueReusableCell(
             withReuseIdentifier: SquareCell.identifier,
             for: indexPath)
             as! SquareCell
+        cell.indexPath = indexPath
         return cell
     }
-    
-    
 }
 
 extension BoardViewController: UICollectionViewDelegateFlowLayout {
-    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath)
+        -> CGSize
+    {
+        let side = collectionView.bounds.width / 8
+        return CGSize(width: side, height: side)
+    }
 }
 
 class SquareCell: UICollectionViewCell {
     static let identifier = "SquareCell"
+    
+    var rank: Int { return indexPath.row % 8 }
+    var file: Int { return indexPath.row / 8 }
+    var isBackgroundLight: Bool { return (rank + file) % 2 == 0 }
+    
+    var indexPath: IndexPath! {
+        didSet {
+            backgroundColor = isBackgroundLight
+                ? UIColor.white
+                : UIColor.black
+            
+        }
+    }
 }
